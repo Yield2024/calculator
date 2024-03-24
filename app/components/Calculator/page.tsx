e5834f0801e364f18c4b6ee2fb59e9083a8072ee
Carlos Manuel Escorcia Polo <carlos@livedesign.org>
2024-03-24 01:46:00 -0500

    Fix text

--- app/components/Calculator/page.tsx
+++ app/components/Calculator/page.tsx
@@ -243,7 +243,7 @@ export default function Calculator() {
 			<div className={styles.main}>
 				<div className={styles.main_left}>
 					<div className={styles.main_left_title}>
-						Long term depository account details
+						 Term depository account details
 					</div>
 					<form className={styles.main_left_form}>
 						<div>
